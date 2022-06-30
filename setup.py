6db406861622d4a45fdf67a814bf91c057b2d9eb
Steven Ros <steven.ros@kwrwater.nl>
2022-06-30 14:05:00 +0200

    greta v0.2

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def read(fname):
 
 setup(
     name='greta',
-    version='0.2.3',
+    version='0.2',
     packages=find_packages(exclude=['tests*']),
     license='MIT',
     description='A python package for correction, validation and analysis of ground water quality samples',
