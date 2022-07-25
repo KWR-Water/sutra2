592eeb2e6696f71b644d4d8213af20b43b0ce9a8
Steven Ros <steven.ros@kwrwater.nl>
2022-07-25 15:48:58 +0200

    v0_32

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def read(fname):
 
 setup(
     name='sutra2',
-    version='0.3.3',
+    version='0.3.2',
     packages=find_packages(exclude=['tests*']),
     license='MIT',
     description='A python package for correction, validation and analysis of ground water quality samples',
